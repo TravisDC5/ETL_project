b71a168a64a53ab736b7ee3c9320ee593f013f2a
TravisDC5 <61341968+TravisDC5@users.noreply.github.com>
2020-06-10 22:02:25 -0700

    worked on python query

--- Resources/testFunction.py
+++ Resources/testFunction.py
@@ -7,10 +7,13 @@ database = pd.read_csv(filelink)
 def querysearch(startdate, enddate, dataframe = database):
     aquired_df = dataframe[dataframe['invoicedate'] >= startdate]
     aquired_df2 = aquired_df[aquired_df['invoicedate'] <= enddate]
-
-    return(print(aquired_df2))
+    aquired_df3 = aquired_df2.groupby(['consignorid'], as_index=False)['itemcost'].sum()
+    #aquired_df4 = aquired_df2.groupby('consignorid')['rent'].avg()
+    return(print(aquired_df3))
 
 #querysearch("2019-08-01", "2019-08-31")
 
-startdate = input(f'')
-enddate = input(f'')
+startdate = input(f'Please input a Starting Date in YYYY-MM-DD format: ')
+enddate = input(f'Please input a Ending Date in YYYY-MM-DD format: ')
+
+querysearch(str(startdate), str(enddate))
